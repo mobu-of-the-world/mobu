c11ef9e1274927cc8e98c58ebc5a3f9c876cb311
pankona <yosuke.akatsuka@gmail.com>
2020-01-01 23:26:31 +0900

    yarn build and rename it to docs

---
+++ docs/precache-manifest.c81e8cba746a289c4b4cbedb79f56727.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3266ebead4faa32222b8a78b376baa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8112583bf1c0848e362",
+    "url": "/static/js/2.07d64906.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.07d64906.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa3bee740775c503c1b2",
+    "url": "/static/js/main.51bfbf23.chunk.js"
+  },
+  {
+    "revision": "cc6cb05fad859dad36f7",
+    "url": "/static/js/runtime-main.8a52f9b7.js"
+  }
+]);
