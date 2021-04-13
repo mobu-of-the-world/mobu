92f464a801b589f68f732cad356e4dede9524c27
pankona <yosuke.akatsuka@gmail.com>
2021-04-13 14:10:43 +0900

    use JSX.IntrinsicElements for Checkbox component

--- src/atoms/Checkbox.tsx
+++ src/atoms/Checkbox.tsx
@@ -1,27 +1,7 @@
-import React, { FC } from "react";
+import { FC } from "react";
 
-type Props = {
-  className?: string;
-  name: string;
-  defaultChecked?: boolean;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-};
-
-const Checkbox: FC<Props> = ({
-  className,
-  name,
-  defaultChecked = false,
-  onChange = () => undefined,
-}) => {
-  return (
-    <input
-      className={className}
-      type="checkbox"
-      name={name}
-      defaultChecked={defaultChecked}
-      onChange={onChange}
-    />
-  );
+const Checkbox: FC<JSX.IntrinsicElements["input"]> = (props) => {
+  return <input {...props} />;
 };
 
 export default Checkbox;
