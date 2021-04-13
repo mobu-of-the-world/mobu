514b5e17112b586c51f4f2969daf65a9474a859c
pankona <yosuke.akatsuka@gmail.com>
2021-04-13 14:19:21 +0900

    add missing type specifier

--- src/atoms/Checkbox.tsx
+++ src/atoms/Checkbox.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
 const Checkbox: FC<JSX.IntrinsicElements["input"]> = (props) => {
-  return <input {...props} />;
+  return <input type="checkbox" {...props} />;
 };
 
 export default Checkbox;
