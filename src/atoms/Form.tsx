94966b663ecbce1cb2aa390ba57ef728383bba52
pankona <yosuke.akatsuka@gmail.com>
2021-12-03 11:00:41 +0900

    yarn prettier:fix

--- src/atoms/Form.tsx
+++ src/atoms/Form.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-const Form: React.FunctionComponent<React.FormHTMLAttributes<HTMLFormElement>> =
-  ({ children, ...rest }) => {
-    return <form {...rest}>{children}</form>;
-  };
+const Form: React.FunctionComponent<
+  React.FormHTMLAttributes<HTMLFormElement>
+> = ({ children, ...rest }) => {
+  return <form {...rest}>{children}</form>;
+};
 
 export default Form;
