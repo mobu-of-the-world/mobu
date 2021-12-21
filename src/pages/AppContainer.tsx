5592968878711da70e181c0cf4db6824d9b3b0da
pankona <yosuke.akatsuka@gmail.com>
2021-12-21 14:46:05 +0900

    fix eslint warning

--- src/pages/AppContainer.tsx
+++ src/pages/AppContainer.tsx
@@ -206,7 +206,7 @@ const numberToTimeString = (count: number): string => {
   return elapsedTime.toISOString().substr(11, 8);
 };
 
-const shuffleArray = <T extends unknown>(array: T[]): T[] => {
+const shuffleArray = <T,>(array: T[]): T[] => {
   for (let i = array.length - 1; i > 0; i--) {
     const rand = Math.floor(Math.random() * (i + 1));
     [array[i], array[rand]] = [array[rand], array[i]];
