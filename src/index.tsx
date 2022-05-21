dd355d7755d57a16976763cb138cbfe95be9c0b9
Kenichi Kamiya <kachick1@gmail.com>
2022-05-21 14:07:11 +0900

    Prefer createRoot rather than render for React 18
    https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import AppContainer from "./pages/AppContainer";
 
 import "ress";
 import "./index.css";
 
-ReactDOM.render(<AppContainer />, document.getElementById("root"));
+createRoot(document.getElementById("root")!).render(<AppContainer />);
