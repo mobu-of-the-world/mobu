381b856dc4364807cd6c4490823bbd36cd6bbd7e
Kenichi Kamiya <kachick1@gmail.com>
2022-05-31 17:46:41 +0900

    Use React.StrictMode as latest template
    * Prefer function style rather than arrow function for using TypeScript generics.
    * Do not use AssertionError. It is defined only in Node.js. None in browser.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import AppContainer from "./pages/AppContainer";
 
 import "ress";
 import "./index.css";
+import { assertIsDefined } from "./utils/typeguard";
 
-createRoot(document.getElementById("root")!).render(<AppContainer />);
+const root = document.getElementById("root");
+assertIsDefined<typeof root>(root);
+createRoot(root).render(
+  <React.StrictMode>
+    <AppContainer />
+  </React.StrictMode>
+);
