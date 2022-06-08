b195b97ea189231f2ef26f841ed9597657705179
Kenichi Kamiya <kachick1@gmail.com>
2022-06-09 00:30:37 +0900

    Reduce needless date broken

--- src/session/timeHelpers.ts
+++ src/session/timeHelpers.ts
@@ -1,5 +1,4 @@
 export const readableElapsedTime = (seconds: number): string => {
-  const date = new Date(0);
-  date.setSeconds(seconds);
+  const date = new Date(seconds * 1000);
   return date.toISOString().substring(11, 19);
 };
