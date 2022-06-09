991f0f536e8262adc8a19b22b34c42ccf5983074
Kenichi Kamiya <kachick1@gmail.com>
2022-06-10 01:19:51 +0900

    Use module css for footer

--- src/footer/index.tsx
+++ src/footer/index.tsx
@@ -1,7 +1,7 @@
-import "./footer.css";
+import css from "./footer.module.css";
 
 const Footer = () => (
-  <footer className="footer">
+  <footer className={css["footer"]}>
     rev - {""}
     <a
       href={`https://github.com/mobu-of-the-world/mobu/tree/${APP_COMMIT_REF}`}
