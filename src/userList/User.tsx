63b264d36ab6e62aad02ebf0f0d495816010ce8b
pankona <yosuke.akatsuka@gmail.com>
2022-06-30 08:40:09 +0900

    replace emojiNameByRole function with simple ternary operation

--- src/userList/User.tsx
+++ src/userList/User.tsx
@@ -6,18 +6,11 @@ import css from "./User.module.css";
 const User = ({ isDriver, user }: { isDriver: boolean; user: string }) => {
   return (
     <div className={css["user"]}>
-      <Emoji emojiName={emojiNameByRole(isDriver)} />
+      <Emoji emojiName={isDriver ? "Car" : "Speaker"} />
       <UserProfileIcon user={user} />
       <Text>{user}</Text>
     </div>
   );
-};
-
-const emojiNameByRole = (isDriver: boolean) => {
-  if (isDriver) {
-    return "Car";
-  }
-  return "Speaker";
 };
 
 export default User;
