429d537a1cfcb706edba03de255cb52f025f5d39
Kenichi Kamiya <kachick1@gmail.com>
2022-11-25 00:18:44 +0900

    Integrate firebase for deployment (#727)
    * Integrate firebase
    
    Basically imported from https://github.com/kachick/convert-color-json-between-windows-terminal-and-vscode/blob/3f97620134b8048d227eae08f0dc81ecc357d59e/.github/workflows/firebase-hosting-pull-request.yml
    
    * Update documents
    
    * `dprint fmt`
    
    * Fix revison footer in PRs
    
    * Trancate revision commit ref

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { execSync } from "child_process";
 
-const commitRef = execSync("git rev-parse --short HEAD").toString().trim();
-
-export default defineConfig({
-  build: {
-    outDir: "./build",
-  },
-  plugins: [react()],
-  define: {
-    APP_COMMIT_REF: JSON.stringify(commitRef),
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const commitRef = env["VITE_COMMIT_REF"] ?? execSync("git rev-parse --short HEAD").toString().trim();
+  const commitShortRef = commitRef.slice(
+    0,
+    7,
+  );
+  return {
+    build: {
+      outDir: "./build",
+    },
+    plugins: [react()],
+    define: {
+      APP_COMMIT_REF: JSON.stringify(commitShortRef),
+    },
+  };
 });
