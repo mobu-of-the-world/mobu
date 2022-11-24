be9934daeb0cc0df6ac2ab6df0f20b8611f23491
Kenichi Kamiya <kachick1@gmail.com>
2022-11-25 00:11:03 +0900

    Trancate revision commit ref

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,17 @@ import { execSync } from "child_process";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   const commitRef = env["VITE_COMMIT_REF"] ?? execSync("git rev-parse --short HEAD").toString().trim();
+  const commitShortRef = commitRef.slice(
+    0,
+    7,
+  );
   return {
     build: {
       outDir: "./build",
     },
     plugins: [react()],
     define: {
-      APP_COMMIT_REF: JSON.stringify(commitRef),
+      APP_COMMIT_REF: JSON.stringify(commitShortRef),
     },
   };
 });
