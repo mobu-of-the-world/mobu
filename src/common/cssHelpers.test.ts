d730d78908df319f1acdeafe51837642f8b220fa
Kenichi Kamiya <kachick1@gmail.com>
2022-11-25 12:27:31 +0900

    Standardize to describe/it style. It is similar to jest and will fit for subtests

--- src/common/cssHelpers.test.ts
+++ src/common/cssHelpers.test.ts
@@ -1,9 +1,11 @@
-import test from "node:test";
+import { describe, it } from "node:test";
 import assert from "node:assert";
 import { buildClassNames } from "./cssHelpers";
 
-void test("returns combined classnames as a string that trimmed empty", () => {
-  assert.strictEqual(buildClassNames(["button", "button-primary"]), "button button-primary");
-  assert.strictEqual(buildClassNames(["button", undefined]), "button");
-  assert.strictEqual(buildClassNames(["button", undefined, "hidden"]), "button hidden");
+describe("buildClassNames", () => {
+  it("returns combined classnames as a string that trimmed empty", () => {
+    assert.strictEqual(buildClassNames(["button", "button-primary"]), "button button-primary");
+    assert.strictEqual(buildClassNames(["button", undefined]), "button");
+    assert.strictEqual(buildClassNames(["button", undefined, "hidden"]), "button hidden");
+  });
 });
--- src/session/timeHelpers.test.ts
+++ src/session/timeHelpers.test.ts
@@ -1,10 +1,12 @@
 import { readableElapsedTime } from "./timeHelpers";
-import test from "node:test";
+import { describe, it } from "node:test";
 import assert from "node:assert";
 
-void test("returns readable format", () => {
-  assert.strictEqual(readableElapsedTime(42), "00:00:42");
-  assert.strictEqual(readableElapsedTime(142), "00:02:22");
-  assert.strictEqual(readableElapsedTime(1001), "00:16:41");
-  assert.strictEqual(readableElapsedTime(60000), "16:40:00");
+describe("readableElapsedTime", () => {
+  it("returns readable format", () => {
+    assert.strictEqual(readableElapsedTime(42), "00:00:42");
+    assert.strictEqual(readableElapsedTime(142), "00:02:22");
+    assert.strictEqual(readableElapsedTime(1001), "00:16:41");
+    assert.strictEqual(readableElapsedTime(60000), "16:40:00");
+  });
 });
