766b4521a44b96dfdd1c6e34690379609a18f188
Kenichi Kamiya <kachick1@gmail.com>
2023-07-13 15:14:27 +0900

    Consider nodejs builtin tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 import { execSync } from "child_process";
 
 export default defineConfig(({ mode }) => {
