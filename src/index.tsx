ae9daa59a8af728cc6db8f80554f51cb67207d38
Kenichi Kamiya <kachick1@gmail.com>
2024-11-28 02:02:09 +0900

    Replace ress with modress

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-import "ress";
 import "./index.css";
 import { assertIsDefined } from "./common/typeguard";
 
